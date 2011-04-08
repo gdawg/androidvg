d3be68f4c0925322a7081eff8c80ba5834d4b305
timestop.tw@gmail.com <timestop.tw@gmail.com@ff550c2b-6b99-7f20-6b93-5bccdf7f204f>
2011-04-08 11:03:32 +0000

    Fix the memory leak problem, which caused by unfree memory used by "vertices" in "shDrawPolygon".

--- jni/shTriangulation.c
+++ jni/shTriangulation.c
@@ -32,47 +32,6 @@
 /*-----------------------------------------------------------
  * Triangulate a cancave polygon 
  *-----------------------------------------------------------*/
-int monotone_check(SHVertex *vertices, int nvertices)
-{
-	int i, j, k;
-	int leftmost_vertex = 0;
-	int rightmost_vertex = 0;
-	SHVector2 leftmost, rightmost, tmp;
-	leftmost.x = vertices[0].point.x;
-	rightmost.x = vertices[0].point.x;
-	
-	//Find the leftmost & rightmost vertices in the path
-	for(i = 1; i < nvertices; i++){
-		if(vertices[i].point.x < leftmost.x) {
-			leftmost.x = vertices[i].point.x;
-			leftmost_vertex = i;
-		}
-		else if(vertices[i].point.x > rightmost.x) {
-			rightmost.x = vertices[i].point.x;
-			rightmost_vertex = i;
-		}
-	}
-
-	i = leftmost_vertex;
-	tmp.x = vertices[i--].point.x;
-	while(i != rightmost_vertex){
-		if(vertices[i].point.x <= tmp.x)
-			return 0;
-		tmp.x = vertices[i].point.x;
-		i = (i + nvertices -1) % nvertices;
-	} 
-
-	i = leftmost_vertex;
-        tmp.x = vertices[i++].point.x;
-	while(i != rightmost_vertex){
-
-		if(vertices[i].point.x <= tmp.x)
-                        return 0;
-        tmp.x = vertices[i].point.x;
-        i = (i + 1) % nvertices;
-    }
-	return 1;
-}
  
  int LeftOnRight(SHVector2 a, SHVector2 b, SHVector2 c )
 {
@@ -84,7 +43,7 @@ int monotone_check(SHVertex *vertices, int nvertices)
 	else 			return 1;//right
 }
  
- void shMonoTriangulation(SHVertex *vertices, int nvertices, SHVector2 *triangles, int ntriangles)
+ void shTriangulation(SHVertex *vertices, int nvertices, SHVector2 *triangles, int ntriangles)
 {
 	//LOGD("Monotone polygon triangulation");
 	// * Not finish yet
@@ -145,7 +104,6 @@ int monotone_check(SHVertex *vertices, int nvertices)
 	}
 
 	//starting tiangulation
-	//int *tri = triangles;
 	j = 0;
 	int *stack = (int*)malloc(nvertices * sizeof(int));
 	int top, current;
@@ -206,97 +164,6 @@ int monotone_check(SHVertex *vertices, int nvertices)
 	free(stack);
 }
   
-void shTriangulation(SHVertex *vertices, int nvertices, SHVector2 *triangles, int ntriangles)
-{
-	//LOGD("Ear-clipping triangulation");
-	SHVector2 current, left, right, internal;
-	SHVector2 s1, s2, s3;
-	int *plunk;
-	int i, j, k;
-	int cross;
-	plunk = (int *) malloc(nvertices * sizeof(int));
-	for (i = 0; i < nvertices; i++) {
-		plunk[i] = 0;
-	}
-	for (i = 0; i < ntriangles; i++) {
-		// Find an ear triangle and plunk it.
-		for (j = 0; j < nvertices; j++) {
-			if (plunk[j] == 1) {
-				continue;
-			}
-			// Find 1st point of the triangle
-			current.x = vertices[j].point.x;
-			current.y = vertices[j].point.y;
-			// Find 2nd point of the triangle
-			k = j;
-			do {
-				if (k == nvertices) {
-					k = 0;
-				} else {
-					k++;
-				}
-				if (plunk[k] == 0) {
-					right.x = vertices[k].point.x;
-					right.y = vertices[k].point.y;
-					break;
-				}
-			} while (k != j);
-			// Find 3rd point of the triangle
-			k = j;
-			do {
-				if (k == -1) {
-					k = nvertices - 1;
-				} else {
-					k--;
-				}
-				if (plunk[k] == 0) {
-					left.x = vertices[k].point.x;
-					left.y = vertices[k].point.y;
-					break;
-				}
-			} while (k != j);
-			// Test triangle(left, current, right) form an
-			// ear triangle or not
-			internal.x = (left.x + right.x) /2;
-			internal.y = (left.y + right.y) /2;
-			for (k = 0, cross = 0; k < nvertices; k++) {
-				// Pick a segment, (s1, s2)
-				s1.x = vertices[k].point.x;
-				s1.y = vertices[k].point.y;
-				if ((k + 1) == nvertices) {
-					s2.x = vertices[0].point.x;
-					s2.y = vertices[0].point.y;
-				} else {
-					s2.x = vertices[k + 1].point.x;
-					s2.y = vertices[k + 1].point.y;
-				}
-				if (((s1.y > internal.y) && (s2.y < internal.y))
-					|| ((s1.y < internal.y) && (s2.y > internal.y))){
-					s3.y = internal.y;
-					s3.x = (s3.y - s1.y) * (s1.x - s2.x) / (s1.y - s2.y) + s1.x;
-					if (s3.x >= internal.x) {
-						cross++;
-					}
-				}
-			}
-			if ((cross % 2) == 1) {
-				// Internal(x,y) is the real internal point
-				// We find one ear triangle 
-				// Plunk it and assign to ouput triangle array
-				plunk[j] = 1;	
-				triangles[i * 3].x = current.x;
-				triangles[i * 3].y = current.y;
-				triangles[i * 3 + 1].x = left.x;
-				triangles[i * 3 + 1].y = left.y;
-				triangles[i * 3 + 2].x = right.x;
-				triangles[i * 3 + 2].y = right.y;
-				break;
-			}
-		}
-	}
-	free(plunk);
-}
-
 /*-----------------------------------------------------------
  * Draws the subdivided vertices to from a polygon in the OpenGL mode given
  * (this could be VG_TRIANGLE_FAN or VG_LINE_STRIP).
@@ -323,18 +190,13 @@ void shDrawPolygon(SHPath *p, GLenum mode)
 			vertices[i].point.x = p->vertices.items[start + i].point.x;
 			vertices[i].point.y = p->vertices.items[start + i].point.y;
 		}
-		shMonoTriangulation(vertices, size, temp, ntris);
-		/*
-		if(monotone_check(vertices, size)){
-			shMonoTriangulation(vertices, size, temp, ntris);
-		}
-		else{
-			shTriangulation(vertices, size, temp, ntris);
-		}*/
+		shTriangulation(vertices, size, temp, ntris);
+
 		// Draw the triangluated subsegment
 		glVertexPointer(2, GL_FLOAT, sizeof(SHVector2), temp);
 		glDrawArrays(GL_TRIANGLES, 0, 3 * ntris);
 		free(temp);
+		free(vertices);
 		start += size;
 	}
 	glDisableClientState(GL_VERTEX_ARRAY);
